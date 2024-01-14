5996a3b936c3aac0f019fec8e06595cf27e22896
hayakawakoichi <koichi.hayakawa.dev@gmail.com>
2024-01-14 15:55:35 +0900

    add clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { SignInButton, UserButton } from '@clerk/nextjs'
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Button>Button</Button>
+            <div className="h-screen">
+                <UserButton afterSignOutUrl="/" />
+                <SignInButton afterSignInUrl="/todo" mode="modal" />
+            </div>
         </main>
     )
 }
---
+++ app/todo/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <main>
+            <h1>TODO APP</h1>
+            {children}
+        </main>
+    )
+}
